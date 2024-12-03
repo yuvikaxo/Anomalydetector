cafccff1b6bbd4e4f98aabddf1e807a57e1a9e5e
yuvikaxo <yuvika.hay@gmail.com>
2024-12-04 02:15:16 +0530

    changes in main file

--- app.py
+++ app.py
@@ -213,4 +213,5 @@ def acc():
     return render_template('acc.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
